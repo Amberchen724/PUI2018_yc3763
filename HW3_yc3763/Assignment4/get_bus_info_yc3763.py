187af9e41ea55c0d847641b8befe0d9e391b4d5e
Yushi Chen <yc3763@ip-10-10-1-15.ec2.internal>
2018-09-25 22:44:06 -0400

    Move Assignment4

--- HW3_yc3763/Assignment4/get_bus_info_yc3763.py
+++ HW3_yc3763/Assignment4/get_bus_info_yc3763.py
@@ -1,7 +1,9 @@
+from __future__ import print_function
 import csv
 import pylab as pl
 import os 
 import json
+
 try:
     import urllib2 as urllib
 except ImportError:
@@ -9,6 +11,7 @@ except ImportError:
 import os
 import sys
 import pandas as pd
+
 url ='http://bustime.mta.info/api/siri/vehicle-monitoring.json?key='+sys.argv[1]+'&VehicleMonitoringDetailLevel=calls&LineRef='+sys.argv[2]
 print(url)
 df = pd.read_json(url)
@@ -16,18 +19,32 @@ df.to_csv("busline.csv")
 response = urllib.urlopen(url)
 data = response.read().decode("utf-8")
 dataDict = json.loads(data)
+
+
 variable1 = dataDict['Siri']['ServiceDelivery']['VehicleMonitoringDelivery'][0]['VehicleActivity']
 variable1 
 index = 0
 df_all=pd.DataFrame(columns=['latitude','Longitude','Busstop','Stopstatus'])
+
 for k in variable1:
     k = k['MonitoredVehicleJourney']
-    latitude = k['VehicleLocation']['Latitude']
-    Longitude = k['VehicleLocation']['Longitude']
-    Busstop = k['OnwardCalls']['OnwardCall'][0]['StopPointName']
-    Stopstatus= k['OnwardCalls']['OnwardCall'][0]['Extensions']['Distances']['PresentableDistance']
-    index+=1 
-    df_all.loc[index] = [latitude,Longitude,Busstop,Stopstatus]
-    print (latitude,Longitude,Busstop,Stopstatus)
+    if len(k['OnwardCalls'])<1:
+        latitude = k['VehicleLocation']['Latitude']
+        Longitude = k['VehicleLocation']['Longitude']
+        Busstop = 'N/A'
+        Stopstatus = 'N/A'
+    else: 
+        latitude = k['VehicleLocation']['Latitude']
+        Longitude = k['VehicleLocation']['Longitude']
+        Busstop = k['OnwardCalls']['OnwardCall'][0]['StopPointName']
+        Stopstatus= k['OnwardCalls']['OnwardCall'][0]['Extensions']['Distances']['PresentableDistance']       
+    index += 1
+    df_all.loc[index]= [latitude,Longitude,Busstop,Stopstatus]
+    print(latitude,Longitude,Busstop,Stopstatus)
+    
 df_all
-df_all.to_csv('bustime.csv')
+df_all.to_csv(sys.argv[3], sep=',') 
+          
+    
+    
+   
